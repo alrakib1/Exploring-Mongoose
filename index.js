3d7981c033999c7ad9287b0cc4f998396fefcb74
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2023-12-20 23:08:49 +0600

    succesfully got data

--- index.js
+++ index.js
@@ -6,6 +6,24 @@ const app = express();
 
 const port = 4000;
 
+app.use(express.json());
+
+// create a product schema
+
+const productsSchema = new mongoose.Schema({
+  title: String,
+  price: Number,
+  description: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// create product model
+
+const Product = mongoose.model("Products", productsSchema);
+
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.url);
@@ -19,4 +37,29 @@ const connectDB = async () => {
 app.listen(port, async () => {
   console.log(`server running on port : ${port}`);
   await connectDB();
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to home page of the server");
+});
+
+app.post("/products", (req, res) => {
+  try {
+
+    
+// get data from req body
+
+    const title = req.body.title;
+    const price = req.body.price;
+    const description = req.body.description;
+
+
+
+
+
+
+    res.status(201).send({title, price, description});
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
 });
