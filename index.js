4a6df03f36533821f34f95a2743f5b296605c498
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2023-12-26 01:47:52 +0600

    learned logical operator

--- index.js
+++ index.js
@@ -34,19 +34,23 @@ const productsSchema = new mongoose.Schema({
 
 const Product = mongoose.model("Products", productsSchema);
 
+// connect to mongodb using function
+
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.url);
-    console.log("db is connected");
+    console.log("Mongodb is connected");
   } catch (error) {
     console.log(error.message);
     process.exit(1);
   }
 };
 
+//
+
 app.listen(port, async () => {
   console.log(`server running on port : ${port}`);
-  await connectDB();
+  await connectDB(); //connect to db after server has started
 });
 
 app.get("/", (req, res) => {
@@ -132,7 +136,7 @@ app.get("/less", async (req, res) => {
     let products;
     // console.log(req.query)
     if (price) {
-      products = await Product.find({ price: { $gt: price } });
+      products = await Product.find({ price: { $lt: price } });
     } else {
       products = await Product.find();
     }
@@ -154,3 +158,80 @@ app.get("/less", async (req, res) => {
 //  we can add custom value to get products as per requirement with query. for example here we get the products that are less 300 .  have used $lt query for it.
 
 // we can use gt to $get value that are greater than 300. If we want exactly same thing then we use $eq
+
+// logical operator
+
+// lets say if we need multiple condition to find data then we can use logical operator.  all logic must be inside an array as an array of object and wrap it with another object.
+
+// $and operator
+
+// In this case, I want to search a product which has price of 500 or more and rating of 4 or more. the logic will be:
+
+// {$and:[{ price: { $gte: price } },{rating: {$gte: 4}}]}    //simply pass this condition to find operator and see . all logic have to be true for this response
+
+app.get("/and", async (req, res) => {
+  try {
+    // const price = req.query.price;
+
+    const price = 400;
+
+    let products;
+    // console.log(req.query)
+    if (price) {
+      products = await Product.find({
+        $and: [{ price: { $gte: price } }, { rating: { $gte: 4 } }],
+      });
+    } else {
+      products = await Product.find();
+    }
+    if (products) {
+      return res.status(200).send({
+        success: true,
+        message: "return all products",
+        data: products,
+      });
+    }
+  } catch (error) {
+    res.status(404).send({
+      success: false,
+      message: "Products not found",
+    });
+  }
+});
+
+// $or operator
+
+// if we have multiple condition and if anyone of them is true then it will return response
+
+// {$or:[{ price: { $gte: price } },{rating: {$gte: 4}}]}     //if any of the one condition is true then it will send the data
+
+app.get("/or", async (req, res) => {
+  try {
+    // const price = req.query.price;
+
+    const price = 400;
+
+
+    let products;
+    // console.log(req.query)
+    if (price) {
+      products = await Product.find({
+        $or: [{ price: { $gte: price } }, { rating: { $gte: 4 } }],
+      });
+    } else {
+      products = await Product.find();
+    }
+    if (products) {
+      return res.status(200).send({
+        success: true,
+        message: "return all products",
+        data: products,
+      });
+    }
+  } catch (error) {
+    res.status(404).send({
+      success: false,
+      message: "Products not found",
+    });
+  }
+});
